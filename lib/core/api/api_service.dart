54ece2e1fa05ee7dedaa339eb39928a0c7870a01
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-03-25 18:07:17 -0400

    implement ApiService with login and register methods; update dependencies and remove unused files

---
+++ lib/core/api/api_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_route/core/api/end_points.dart';
+import 'package:ecommerce_route/core/api/models/request/login_request_dto.dart';
+import 'package:ecommerce_route/core/api/models/request/register_request_dto.dart';
+import 'package:ecommerce_route/core/api/models/response/auth_response_dto.dart';
+
+import 'package:retrofit/retrofit.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: EndPoints.baseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String? baseUrl}) = _ApiService;
+
+  @POST(EndPoints.login)
+  Future<AuthResponseDto> login(@Body() LoginRequestDto loginRequestDto);
+
+  @POST(EndPoints.register)
+  Future<AuthResponseDto> register(@Body() RegisterRequestDto registerRequestDto);
+}
