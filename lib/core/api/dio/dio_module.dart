4ef3d5d843ce305b704437b0cd16b5d9a79ca2ff
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-03-25 18:36:49 -0400

    add dependency injection for Dio and ApiService; implement PrettyDioLogger in GetItModule

---
+++ lib/core/api/dio/dio_module.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_route/core/api/api_service.dart';
+import 'package:ecommerce_route/core/api/end_points.dart';
+import 'package:injectable/injectable.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+@module
+abstract class GetItModule {
+  var dio = Dio();
+  @singleton
+  @injectable
+  BaseOptions provideBaseOptions() => BaseOptions(
+    baseUrl: EndPoints.baseUrl,
+    receiveDataWhenStatusError: true,
+    connectTimeout: const Duration(seconds: 20),
+    receiveTimeout: const Duration(seconds: 20),
+  );
+
+  @singleton
+  @injectable
+  PrettyDioLogger providePrettyDioLogger() => PrettyDioLogger(
+    requestHeader: true,
+    requestBody: true,
+    responseBody: true,
+    responseHeader: true,
+    error: true,
+    compact: true,
+    maxWidth: 90,
+  );
+  @singleton
+  @injectable
+  Dio provideDio(BaseOptions baseOptions, PrettyDioLogger prettyDioLogger) {
+    var dio = Dio(baseOptions);
+    //! TODO: Add Interceptors
+    dio.interceptors.add(prettyDioLogger);
+    return dio;
+  }
+
+@singleton
+@injectable
+  ApiService provideApiService(Dio dio) => ApiService(dio);
+}
